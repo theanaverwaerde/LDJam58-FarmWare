a6f768af3411143711818f73814d3f44ade2fc99
theana <41345545+theanaverwaerde@users.noreply.github.com>
2025-10-04 18:17:51 +0200

    Add more structure

--- game/main.lua
+++ game/main.lua
@@ -1,57 +1,22 @@
-Wheats = {}
-WheatImg = love.graphics.newImage("assets/wheat.png")
-ScytheImg = love.graphics.newImage("assets/scythe.png")
-RemainingWheats = 25
+Object = require "lib.classic"
+Scene = {}
+
+local games  = {
+	require "wheatGame",
+}
 
 function love.load()
-	love.mouse.setVisible(false)
-	resetGame()
+	Scene = games[1]()
 end
 
 function love.update(dt)
-	if RemainingWheats == 0 then
-		print("You win!")
-		if love.keyboard.isDown("r") then
-			resetGame()
-		end
-		love.window.setTitle("You win! Press R to reset.")
-	else
-		love.window.setTitle("Wheats remaining: " .. RemainingWheats)
-		for i=1,25 do
-			if Wheats[i] then
-				local row = math.floor((i-1)/5)
-				local col = (i-1)%5
-				local wheatX = col * 64 + 100
-				local wheatY = row * 64 + 100
-				local mouseX = love.mouse.getX()
-				local mouseY = love.mouse.getY()
-				if mouseX > wheatX and mouseX < wheatX + 64 and mouseY > wheatY and mouseY < wheatY + 64 then
-					Wheats[i] = false
-					RemainingWheats = RemainingWheats - 1
-				end
-			end
-		end
-	end
-end
-
-function resetGame()
-	Wheats = {}
-	for i=1,25 do
-		Wheats[i] = true
-	end
-	RemainingWheats = 25
+	Scene:update(dt)
 end
 
 function love.draw()
-	love.graphics.setBackgroundColor(0.2, 0.7, 0.1)
+	Scene:draw()
+end
 
-	for i=1,25 do
-		if Wheats[i] then
-			local row = math.floor((i-1)/5)
-			local col = (i-1)%5
-			love.graphics.draw(WheatImg, col * 64 + 100, row * 64 + 100)
-		end
-	end
-
-	love.graphics.draw(ScytheImg, love.mouse.getX(), love.mouse.getY())
+function NextScene()
+	Scene = games[math.random(1, #games)]()
 end
---
+++ game/wheatGame.lua
@@ -0,0 +1,56 @@
+local WheatGame = Object.extend(Object)
+
+function WheatGame:new()
+	love.mouse.setVisible(false)
+
+	self.wheats = {}
+	self.wheatImg = love.graphics.newImage("assets/wheat.png")
+	self.scytheImg = love.graphics.newImage("assets/scythe.png")
+	self.remainingWheats = 25
+
+	for i=1,25 do
+		self.wheats[i] = true
+	end
+end
+
+function WheatGame:update(dt)
+	if self.remainingWheats == 0 then
+		print("You win!")
+		love.window.setTitle("You win! Click to continue.")
+		if love.mouse.isDown(1) then
+			NextScene()
+		end
+	else
+		love.window.setTitle("Wheats remaining: " .. self.remainingWheats)
+		for i=1,25 do
+			if self.wheats[i] then
+				local row = math.floor((i-1)/5)
+				local col = (i-1)%5
+				local wheatX = col * 64 + 100
+				local wheatY = row * 64 + 100
+				local mouseX = love.mouse.getX()
+				local mouseY = love.mouse.getY()
+				if mouseX > wheatX and mouseX < wheatX + 64 and mouseY > wheatY and mouseY < wheatY + 64 then
+					self.wheats[i] = false
+					self.remainingWheats = self.remainingWheats - 1
+				end
+			end
+		end
+	end
+end
+
+function WheatGame:draw()
+	love.graphics.setBackgroundColor(0.2, 0.7, 0.1)
+
+	for i=1,25 do
+		if self.wheats[i] then
+			local row = math.floor((i-1)/5)
+			local col = (i-1)%5
+			love.graphics.draw(self.wheatImg, col * 64 + 100, row * 64 + 100)
+		end
+	end
+
+	love.graphics.draw(self.scytheImg, love.mouse.getX(), love.mouse.getY())
+end
+
+return WheatGame
